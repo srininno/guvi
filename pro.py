ebea6907fb67f93224c064ac9a46778a5e9c6ba0
srininno <42862890+srininno@users.noreply.github.com>
2019-02-08 14:40:09 +0530

    Create pro.py

---
+++ pro.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=[]
+for i in range(n):
+    a.apppend(input())
+ans=a[0]
+for i in a:
+    al=""
+    for j in range(len(ans)):
+        if i[j]!=ans[j]:
+            ans=a1
+            break
+        else:
+            a1+=ans[j]
+print(ans)
