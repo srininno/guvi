142880cb3aa9f143394d452fe141f45329cebd8f
srininno <42862890+srininno@users.noreply.github.com>
2019-02-08 12:01:18 +0530

    Update oddnuminter.py

--- oddnuminter.py
+++ oddnuminter.py
@@ -1,7 +1,9 @@
-#srini
+#srinivas
 a=input().split()
 n=int(a[0])
 q=int(a[1])
+list=[]
 for i in range(n+1,q):
-    if(i%2!=0):
-        print(i,end=" ")
+   if(i%2!=0):
+    list.append(i)
+print(" ".join(map(str,list)))
